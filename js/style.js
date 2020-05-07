8e29cc01bfd992668ff3986bf3289cbd061cc04f
Subhradeep Nag <52783389+subhradeepnag@users.noreply.github.com>
2020-05-07 23:23:28 +0530

    commit

--- js/style.js
+++ js/style.js
@@ -3,20 +3,25 @@ $(document).ready(function() {
   var questionText = ["1. A 10 year old boy presents with a 2 week history of severe fever and profuse sweating, headache, nausea, muscle pain ,bloody stools , diarrhea, vomiting and cough. No toxin ingestion has been noted and the family denies any use of traditional medicines.",
   "2. Raj was 22 year old male presents with symptoms of itching, stinging, and burning between your toes or on soles of your feet , cracking and peeling skin on your feet, most commonly between your toes and dry skin, discolored, thick, and crumbly toenails was discovered.",
   "3. A 16 year old girl presents with a sore throat and hoarseness with enlarged lymph nodes in the  neck region .also with the difficulty in breathing or rapid breathing and Nasal discharge for about 5 days.",
-  "1. For the disease MALARIA the most accurate antibiotic would be ","2. Choose the suitable antibiotic for the patient suffering from the common disease ATHLETE’S FOOT ",
-  "3. A serious bacterial infection which can be spread easily such as DIPHTHERIA can be treated with?","Guess this structure - C22H24N2O8","Guess this structure - C18H14Cl4N2O","Guess this structure - C37H67NO13"]
+  "1. For the disease MALARIA the most accurate antibiotic would be ",
+  "2. Choose the suitable antibiotic for the patient suffering from the common disease ATHLETE’S FOOT ",
+  "3. A serious bacterial infection which can be spread easily such as DIPHTHERIA can be treated with?",
+  "Guess this structure - Doxycycline",
+  "Guess this structure - Miconazole",
+  "Guess this structure - Erythromycin"
+   ]
   var choices = [
-    ["a.  Pneumonia" ,"b.  Malaria" , "c.  Typhoid " , "d.  Hyperthermia"], 
+    ["a.  Pneumonia" ,"b.  Malaria" , "c.  Typhoid " , "d.  Hyperthermia"],
     ["a.  Hammer toe","b.  Vascular ischemia " , "c.  Perniosis", "d.  Athlete’s foot "],
     ["a.  Diphtheria","b.  Throat infection","c.  Viral fever" , "d.  Cholera"],
-    ["a.  Adoxa","b.  Erythromycin","c.  Vibramycin","d.  Doxycycline"], 
+    ["a.  Adoxa","b.  Erythromycin","c.  Vibramycin","d.  Doxycycline"],
     ["a.  Penicillin","b.  Amoxicillin","c.  Miconazole","d.  Quinidine"],
     ["a.  Erythromycin","b.  Zestril","c.  Acetaminophen","d.  Glucophage "],
-    ["a. Doxycycline","b.Amoxicillin" , "c.Vibramycin","d.Penicillin"],
-    ["a.Zestril","b.Miconazole","c.Glucophage","d.Quinidine"],
-    ["a.Doxycyclin","b.Glucophage","c.Erythromycin","d.Adoxa"]
+    ["a. C23H32CIN2O10","b.C25H33CIN2O11" , "c.C24H33CIN2O10","d.C24H34CIN2O11"],
+    ["a.C18H14CIN2O2","b.C24H33CIN2O11","c.C21H34CIN2O11","d.C18H14Cl4N2O"],
+    ["a.C37H67NO13","b.C37H57NO12","c.C34H57NO13","d.C37H64NO15"]
   ]
-  var answers = [2,4,1,4,3,1,1,2,3];
+  var answers = [2,4,1,4,3,1,3,4,1];
   var userAnswers = [0,0,0,0,0,0,0,0,0];
   var ans = 0;
   var correct = 0;
@@ -49,10 +54,10 @@ $(document).ready(function() {
 
   function questionBoxIn(id) {
     $(".question-box h2").text(questionText[id]);
-    $(".question-box").fadeIn(500);
+    $(".question-box").fadeIn(1000);
     console.log(id+".png");
     $(".question-box img").attr("src", id+".png");
-    $(".question-box").fadeIn(500);
+    $(".question-box").fadeIn(1000);
   }
 
   function optionsIn(id) {
@@ -134,7 +139,7 @@ function startQuiz() {
         id++;
         count++;
       }
-      
+
     }
     else if(count==6){
       if(flag2==0){
@@ -164,12 +169,12 @@ function startQuiz() {
       console.log("------------RESULT------------");
       showResult();
     }
-      else{ 
+      else{
         questionOn(count);
         id++;
         count++;
         }
-    
+
   }, 11000);
 }
 
@@ -207,7 +212,7 @@ function showResult() {
     $('#score').text(correct);
     $('.result').fadeIn(1000);
     $('.do').text("You can do better!!");
-  } 
+  }
 }
 
 
